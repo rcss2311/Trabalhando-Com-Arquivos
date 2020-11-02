7e5c8b8985662f462454a3bf661d1f7945d7c5ba
Ricardo Souza <rcss2311@gmail.com>
2020-11-02 18:42:30 -0300

    Try_Catch_with_resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,20 @@
 package application;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 public class Program {
 	public static void main(String[] args) {
 		String path = "C:\\WS-Eclipse\\in.txt";
-		BufferedReader br = null;
-		FileReader fr = null;
-		try {
-				fr = new FileReader(path);
-				br = new BufferedReader(fr);
-				String line = br.readLine();// .read line lê apenas uma linha do codigo
-					while (line != null) {// usando while para ler o arquivo texto, vai ler até line ter valor null
-						System.out.println(line); //printa a linha
-						line = br.readLine();// passa pra proxima linha do arquivo
-					}
-		} catch (IOException e) {
-				System.out.println("Error: " + e.getMessage());
-		} finally {
-		try {
-				if (br != null)
-					br.close();
-				if (fr != null)
-						fr.close();
-		} catch (IOException e) {
-				e.printStackTrace();
-				}
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) { //instanciação ocorre dentro do bloco Tri-Catch with resouces 
+			String line = br.readLine();									 //eliminando a necessidade de de instanciar fora e enxugando o codigo
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 }
