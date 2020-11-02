a39c5479cec9482a31ef2c63d49237da4199c77c
Ricardo Souza <rcss2311@gmail.com>
2020-11-02 19:17:38 -0300

    Informações do caminho do arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,21 +12,11 @@ public class Program {
 		
 		File path = new File(strPath);
 		
-		File[] folders = path.listFiles(File::isDirectory); // criaa um vetor com a lista de todas as pastas
-															//isDirectory pega o caminho de cada pasta e coloca dentro do vetor' 
-		System.out.println("FOLDERS:");
+		System.out.println("getPath: " + path.getPath());//pega o caminho completo
+		System.out.println("getParent: " + path.getParent());// pega o caminho e ignora o arquivo ou nome
+		System.out.println("getName: " + path.getName());// pega apenas o nome do arquivo
 		
-		for (File folder : folders) {
-			System.out.println(folder);
-		}//até aqui, imprime uma lista de pastas contidas no caminho
-		
-		File[] files = path.listFiles(File::isFile);//imprime apenas arquivos com o metodo isFile
-		System.out.println("FILES:");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		boolean success = new File(strPath + "\\subdir").mkdir();//cria uma sub pasta a partir do strPath 
-		System.out.println("Directory created successfully: " + success);
 		sc.close();
+		
 	}
 }
