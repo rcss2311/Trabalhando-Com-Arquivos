104817bce3b81b3635692adaa435ab47d0a6c567
Ricardo Souza <rcss2311@gmail.com>
2020-11-02 18:54:58 -0300

    Filewriter e BifferWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,20 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 	public static void main(String[] args) {
-		String path = "C:\\WS-Eclipse\\in.txt";
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) { //instanciação ocorre dentro do bloco Tri-Catch with resouces 
-			String line = br.readLine();									 //eliminando a necessidade de de instanciar fora e enxugando o codigo
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+		String path = "C:\\WS-Eclipse\\out.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
 		}
 	}
 }
