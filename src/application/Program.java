ed7439feb9bf5f95484c33cf5edf5e350fc2c286
Ricardo Souza <rcss2311@gmail.com>
2020-11-02 18:29:55 -0300

    BufferedReader e FileReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,31 @@
 package application;
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
-		File file = new File("C:\\WS-Eclipse\\in.txt");
-		Scanner sc = null;
+		String path = "C:\\WS-Eclipse\\in.txt";
+		BufferedReader br = null;
+		FileReader fr = null;
 		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-			System.out.println(sc.nextLine());
-			}
-	}
-		catch (IOException e) {
-		System.out.println("Error: " + e.getMessage());
-	}
-		finally {
-			if (sc != null) {
-				sc.close();
+				fr = new FileReader(path);
+				br = new BufferedReader(fr);
+				String line = br.readLine();// .read line lê apenas uma linha do codigo
+					while (line != null) {// usando while para ler o arquivo texto, vai ler até line ter valor null
+						System.out.println(line); //printa a linha
+						line = br.readLine();// passa pra proxima linha do arquivo
+					}
+		} catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+		} finally {
+		try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+						fr.close();
+		} catch (IOException e) {
+				e.printStackTrace();
 				}
 			}
-		}
 	}
+}
